6101f14d0cf93ec62127180b442b39e2b93e37d1
kwonjeong <kwonjeong9470@gmail.com>
2023-11-27 20:21:48 +0900

    #1 feat: 서버통신 초기세팅

---
+++ ShallWe/ShallWe/Network/Base/URLConstant.swift
@@ -0,0 +1,19 @@
+//
+//  URLConstant.swift
+//  ShallWe
+//
+//  Created by KJ on 11/27/23.
+//
+
+import Foundation
+
+enum URLConstant {
+    
+    // MARK: - Base URL
+    
+    static let baseURL = Config.baseURL
+    
+    // MARK: - URL Path
+    
+    
+}
---
+++ ShallWe/ShallWe/Network/Enviornment/Config.swift
@@ -0,0 +1,34 @@
+//
+//  Config.swift
+//  ShallWe
+//
+//  Created by KJ on 11/27/23.
+//
+
+import Foundation
+
+enum Config {
+    
+    enum Keys {
+        enum Plist {
+            static let baseURL = "BASE_URL"
+        }
+    }
+    
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("plist cannot found.")
+        }
+        return dict
+    }()
+}
+
+extension Config {
+    
+    static let baseURL: String = {
+        guard let key = Config.infoDictionary[Keys.Plist.baseURL] as? String else {
+            fatalError("Base URL is not set in plist for this configuration.")
+        }
+        return key
+    }()
+}
