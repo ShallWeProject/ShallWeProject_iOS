786192255ecfc4a986947a0cc99290152c8703d5
kwonjeong <kwonjeong9470@gmail.com>
2023-12-02 23:13:25 +0900

    #3 feat: 상황별 추천 경험 셀 구현

--- ShallWe/ShallWe/Presentation/Home/Cells/HomeRecommendCell.swift
+++ ShallWe/ShallWe/Presentation/Home/Cells/HomeRecommendCell.swift
@@ -7,6 +7,51 @@
 
 import UIKit
 
-class HomeRecommendCell: UICollectionViewCell {
+import SnapKit
+import Then
+
+final class HomeRecommendCell: UICollectionViewCell {
     
+    // MARK: - UI Components
+    
+    private let recommendImageView = UIImageView()
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension HomeRecommendCell {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        
+        self.backgroundColor = .clear
+        self.contentView.backgroundColor = .clear
+        self.makeCornerRound(radius: 10)
+        
+        recommendImageView.do {
+            $0.image = ImageLiterals.Home.img_birthday
+        }
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+        self.contentView.addSubviews(recommendImageView)
+        
+        recommendImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 }
