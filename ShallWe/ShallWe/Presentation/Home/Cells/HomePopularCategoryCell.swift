4d9540b4c1ef84a17298e1eaaf26c966c8da4bd2
kwonjeong <kwonjeong9470@gmail.com>
2023-12-02 23:19:21 +0900

    #3 feat: 실시간 인기 경험 셀 구현

--- ShallWe/ShallWe/Presentation/Home/Cells/HomePopularCategoryCell.swift
+++ ShallWe/ShallWe/Presentation/Home/Cells/HomePopularCategoryCell.swift
@@ -7,6 +7,53 @@
 
 import UIKit
 
-class HomePopularCategoryCell: UICollectionViewCell {
+import SnapKit
+import Then
+
+final class HomePopularCategoryCell: UICollectionViewCell {
     
+    // MARK: - UI Components
+    
+    private let titleLabel = UILabel()
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension HomePopularCategoryCell {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        
+        self.backgroundColor = .clear
+        self.contentView.backgroundColor = .clear
+        self.makeCornerRound(radius: 13)
+        
+        titleLabel.do {
+            $0.text = "전체"
+            $0.font = .fontGuide(.M00_12)
+            $0.textColor = .gray4
+        }
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+        self.contentView.addSubviews(titleLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
 }
