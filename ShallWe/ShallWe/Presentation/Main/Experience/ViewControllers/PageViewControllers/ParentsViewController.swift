b6d7215652f375655283736cfe0c300e36c00ac6
kwonjeong <kwonjeong9470@gmail.com>
2023-12-13 00:56:24 +0900

    #13 feat: 상황별추천 경험 UI 구현

--- ShallWe/ShallWe/Presentation/Main/Experience/ViewControllers/PageViewControllers/ParentsViewController.swift
+++ ShallWe/ShallWe/Presentation/Main/Experience/ViewControllers/PageViewControllers/ParentsViewController.swift
@@ -7,23 +7,44 @@
 
 import UIKit
 
-class ParentsViewController: UIViewController {
+import Then
+import SnapKit
+import RxSwift
+import RxCocoa
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+final class ParentsViewController: BaseViewController {
 
-        // Do any additional setup after loading the view.
+    // MARK: - UI Components
+    
+    private let experienceView = HomeExperienceListView()
+    
+    // MARK: - Properties
+    
+    private let viewModel = HomeExperienceListViewModel()
+    private let disposeBag = DisposeBag()
+    
+    override func bindViewModel() {
+        viewModel.outputs.birthList
+            .bind(to: experienceView.homelistCollectionView.rx
+                .items(cellIdentifier: HomeExperienceCell.className,
+                       cellType: HomeExperienceCell.self)) { (index, model, cell) in
+                    cell.configureCell(model)
+                }
+                .disposed(by: disposeBag)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func setStyle() {
+        
+        self.view.backgroundColor = .clear
     }
-    */
+    
+    override func setLayout() {
+        
+        self.view.addSubviews(experienceView)
+        
+        experienceView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 
 }
