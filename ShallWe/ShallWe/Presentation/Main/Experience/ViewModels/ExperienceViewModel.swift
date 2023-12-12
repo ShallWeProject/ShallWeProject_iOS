d6952e379987fc2f699c56bf05c35e9f46137313
kwonjeong <kwonjeong9470@gmail.com>
2023-12-12 21:16:09 +0900

    #13 feat: 경험 뷰모델 구현

--- ShallWe/ShallWe/Presentation/Main/Experience/ViewModels/ExperienceViewModel.swift
+++ ShallWe/ShallWe/Presentation/Main/Experience/ViewModels/ExperienceViewModel.swift
@@ -5,4 +5,36 @@
 //  Created by KJ on 12/12/23.
 //
 
-import Foundation
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+protocol ExperienceViewModelInputs {
+
+}
+
+protocol ExperienceViewModelOutputs {
+    var recommendMenu: BehaviorRelay<[ExperienceType]> { get }
+    var categoryMenu: BehaviorRelay<[ExperienceType]> { get }
+}
+
+protocol ExperienceViewModelType {
+    var inputs: ExperienceViewModelInputs { get }
+    var outputs: ExperienceViewModelOutputs { get }
+}
+
+final class ExperienceViewModel: ExperienceViewModelInputs, ExperienceViewModelOutputs, ExperienceViewModelType {
+    
+    var recommendMenu: BehaviorRelay<[ExperienceType]> = BehaviorRelay(value: [])
+    var categoryMenu: BehaviorRelay<[ExperienceType]> = BehaviorRelay(value: [])
+    
+    var inputs: ExperienceViewModelInputs { return self }
+    var outputs: ExperienceViewModelOutputs { return self }
+    
+    init(){
+        self.recommendMenu.accept(ExperienceType.recommendMenu())
+        self.categoryMenu.accept(ExperienceType.categoryMenu())
+    }
+
+}
