4c5856f7b9ef2ce6ed2db92ba19403528d72946b
Nya128 <nanalovesbap@gmail.com>
2023-12-21 09:00:21 +0900

    #6 chore: StringLiterals에 텍스트 추가

--- ShallWe/ShallWe/Global/Literals/StringLiterals.swift
+++ ShallWe/ShallWe/Global/Literals/StringLiterals.swift
@@ -36,12 +36,28 @@ enum I18N {
         static let phoneNumberInputText = "전화번호를 입력해주세요"
         static let verificationCodeInputText = "인증번호를 입력해주세요"
         static let verificationRequestText = "인증 요청"
+        static let verificationSuccessText = "인증 완료"
         static let checkText = "인증번호 확인"
         static let nextText = "다음"
         static let loginCompletionText = "로그인 완료!"
         static let signupCompletionText = "회원가입 완료!"
         static let serviceAvailableText = "지금 바로 셸위의 다양한 서비스를\n이용하실 수 있습니다"
         static let goHomeText = "홈으로"
+        static let ageInputText = "나이를 입력해주세요"
+        static let ageUnitText = "세"
+        static let genderInputText = "성별을 선택해주세요"
+        static let maleText = "남성"
+        static let femaleText = "여성"
+        static let uncheckedText = "선택안함"
+        static let totallyAgreeText = "전체 동의"
+        static let age14OrOlderText = "만 14세 이상입니다"
+        static let agreeToTermsOfServiceText = "서비스 이용약관 동의"
+        static let agreeToCollectionAndUsePersonalInfoText = "개인정보 수집 및 이용 동의"
+        static let agreeToReceiveMarketingInfoText = "마케팅 정보 수신 동의"
+        static let requiredText = "필수"
+        static let optionalText = "선택"
+        static let seeDetailText = "내용보기"
+        static let closeText = "닫기"
     }
     
     enum Home {
@@ -50,5 +66,12 @@ enum I18N {
         static let headerTitle1Color = "상황별"
         static let headerTitle2 = "실시간 인기 경험"
         static let headerTitle2Color = "인기"
+    }
+    
+    enum AuthAlert {
+        static let codeSentText = "인증번호가 전송되었습니다."
+        static let verifiedText = "인증되었습니다."
+        static let incorrectCodeText = "인증번호가 일치하지 않습니다. 다시 시도해주세요."
+        static let timeoutText = "인증 시간이 초과되었습니다. 다시 시도해주세요."
     }
 }
