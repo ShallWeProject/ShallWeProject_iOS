8ec973a198e1405ca967130627cfc6616e044e9b
kwonjeong <kwonjeong9470@gmail.com>
2024-01-03 02:20:23 +0900

    #19 add: inset 구하는 extension 추가

--- ShallWe/ShallWe/Global/Extension/UIViewController+.swift
+++ ShallWe/ShallWe/Global/Extension/UIViewController+.swift
@@ -14,5 +14,31 @@ extension UIViewController {
         alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
+    
+    func safeAreaTopInset() -> CGFloat {
+        if #available(iOS 13.0, *) {
+            let window = UIApplication.shared.windows.first
+            let top = window?.safeAreaInsets.top
+            return top ?? 0.0
+            
+        } else if #available(iOS 11.0, *) {
+            let window = UIApplication.shared.keyWindow
+            let top = window?.safeAreaInsets.top
+            return top ?? 0.0
+        }
+    }
+    
+    func safeAreaBottomInset() -> CGFloat {
+        if #available(iOS 13.0, *) {
+            let window = UIApplication.shared.windows.first
+            let bottom = window?.safeAreaInsets.bottom
+            return bottom ?? 0.0
+            
+        } else if #available(iOS 11.0, *) {
+            let window = UIApplication.shared.keyWindow
+            let bottom = window?.safeAreaInsets.bottom
+            return bottom ?? 0.0
+        }
+    }
 }
 
