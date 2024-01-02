3d14c0cc3594d0bc64d3d8d8e3169f42bcc13fcb
kwonjeong <kwonjeong9470@gmail.com>
2024-01-02 19:27:41 +0900

    #19 add: 카테고리 뷰 생성

---
+++ ShallWe/ShallWe/Presentation/Category/Views/CategoryView.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryView.swift
+//  ShallWe
+//
+//  Created by KJ on 1/1/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class CategoryView: BaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
