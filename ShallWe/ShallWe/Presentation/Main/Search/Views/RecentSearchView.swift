113a2591223e0347041cd45e463210746b8f1f96
kwonjeong <kwonjeong9470@gmail.com>
2024-01-03 18:55:45 +0900

    #21 feat: 최근 검색 뷰 생성

---
+++ ShallWe/ShallWe/Presentation/Main/Search/Views/RecentSearchView.swift
@@ -0,0 +1,23 @@
+//
+//  RecentSearchView.swift
+//  ShallWe
+//
+//  Created by KJ on 1/3/24.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+class RecentSearchView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
