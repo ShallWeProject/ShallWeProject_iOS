c5d971eea48d21e3c5a0db274376dec96db42c23
kwonjeong <kwonjeong9470@gmail.com>
2024-01-21 14:52:24 +0900

    [Feat] #24 - 헤더 뷰 타이틀 모델 생성

--- ShallWe/ShallWe/Presentation/Main/Experience/Models/SortModel.swift
+++ ShallWe/ShallWe/Presentation/Main/Experience/Models/SortModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct SortModel {
+    let title: String
+}
+
+extension SortModel {
+    
+    static func sortData() -> [SortModel] {
+        return [
+            SortModel(title: "인기순"),
+            SortModel(title: "추천순"),
+            SortModel(title: "가격높은순"),
+            SortModel(title: "가격낮은순")
+        ]
+    }
+}
