6b8a1923913226f0453dfa02b226f68c821630f0
ahra1221 <lizze011221@gmail.com>
2024-02-10 22:24:49 +0900

    #41 feat: ExperienceTarget 구현

---
+++ ShallWe/ShallWe/Network/APIService/Experience/ExperienceTarget.swift
@@ -0,0 +1,47 @@
+//
+//  ExperienceTarget.swift
+//  ShallWe
+//
+//  Created by 고아라 on 2/10/24.
+//
+
+import Foundation
+
+import Moya
+
+enum ExperienceTarget {
+    case getExperienceDetail(experienceGiftId: Int)
+}
+
+extension ExperienceTarget: TargetType {
+    var baseURL: URL {
+        return URL(string: URLConstant.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .getExperienceDetail(experienceGiftId: let experienceGiftId):
+            let path = URLConstant.experienceDetails
+                .replacingOccurrences(of: "{ExperienceGiftId}", with: String(experienceGiftId))
+            return path
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getExperienceDetail:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getExperienceDetail:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return APIConstants.headerWithAuthorization
+    }
+}
