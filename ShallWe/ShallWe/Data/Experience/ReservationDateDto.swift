0f09b40455b6606799edb748621f015c258dff11
ahra1221 <lizze011221@gmail.com>
2024-02-15 02:54:20 +0900

    #45 feat: ReservationDateDto 구현

---
+++ ShallWe/ShallWe/Data/Experience/ReservationDateDto.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationDateDto.swift
+//  ShallWe
+//
+//  Created by 고아라 on 2/15/24.
+//
+
+import Foundation
+
+struct ReservationDateDto: Codable {
+    let reservationID: Int
+    let status, time: String
+
+    enum CodingKeys: String, CodingKey {
+        case reservationID = "reservationId"
+        case status, time
+    }
+}
