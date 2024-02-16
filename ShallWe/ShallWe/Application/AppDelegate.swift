70167c3b14ffa442ad69d5e739d247612547585f
Nya128 <nanalovesbap@gmail.com>
2024-02-16 18:12:01 +0900

    #40 feat: 카카오 로그인 초기 세팅 완료

--- ShallWe/ShallWe/Application/AppDelegate.swift
+++ ShallWe/ShallWe/Application/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import KakaoSDKCommon
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        KakaoSDK.initSDK(appKey: Config.kakaoNativeAppKey)
+        
         return true
     }
 
--- ShallWe/ShallWe/Network/Enviornment/Config.swift
+++ ShallWe/ShallWe/Network/Enviornment/Config.swift
@@ -12,6 +12,7 @@ enum Config {
     enum Keys {
         enum Plist {
             static let baseURL = "BASE_URL"
+            static let kakaoNativeAppKey = "KAKAO_NATIVE_APP_KEY"
         }
     }
     
@@ -28,6 +29,13 @@ extension Config {
     static let baseURL: String = {
         guard let key = Config.infoDictionary[Keys.Plist.baseURL] as? String else {
             fatalError("Base URL is not set in plist for this configuration.")
+        }
+        return key
+    }()
+    
+    static let kakaoNativeAppKey: String = {
+        guard let key = Config.infoDictionary[Keys.Plist.kakaoNativeAppKey] as? String else {
+            fatalError("Kakao Native App Key is not set in plist for this configuration.")
         }
         return key
     }()
