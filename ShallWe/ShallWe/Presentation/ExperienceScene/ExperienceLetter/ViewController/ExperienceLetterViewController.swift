0dd5af4ef240db80299f1efa568171056ec4a4e8
ahra1221 <lizze011221@gmail.com>
2024-02-24 20:39:42 +0900

    #45 feat: 정규식 추가 및 텍스트필드 처리 구현

--- ShallWe/ShallWe/Presentation/ExperienceScene/ExperienceLetter/ViewController/ExperienceLetterViewController.swift
+++ ShallWe/ShallWe/Presentation/ExperienceScene/ExperienceLetter/ViewController/ExperienceLetterViewController.swift
@@ -9,13 +9,27 @@ import UIKit
 
 final class ExperienceLetterViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    private let giftViewModel: ExperienceGiftViewModel
+    
     // MARK: - UI Components
     
     private let experienceLetterView = ExperienceLetterView()
 
     // MARK: - Life Cycles
     
+    init(viewModel: ExperienceGiftViewModel) {
+        self.giftViewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func loadView() {
+        
         view = experienceLetterView
     }
     
@@ -23,6 +37,8 @@ final class ExperienceLetterViewController: UIViewController {
         super.viewDidLoad()
         
         setUI()
+        setAddTarget()
+        setDelegate()
     }
 }
 
@@ -36,10 +52,74 @@ extension ExperienceLetterViewController {
         experienceLetterView.giftButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     }
     
+    func setDelegate() {
+        experienceLetterView.recipientNameTextField.delegate = self
+        experienceLetterView.senderTextField.delegate = self
+        experienceLetterView.phoneFirstText.delegate = self
+        experienceLetterView.phoneMidText.delegate = self
+        experienceLetterView.phoneEndText.delegate = self
+    }
+    
     @objc
     func buttonTapped() {
         let nav = CompleteViewController()
         nav.fromExperience = true
         self.navigationController?.pushViewController(nav, animated: true)
+    }
+    
+    func validateTextField(textField: UITextField, isValid: Bool) {
+        if isValid {
+            if let customTextField = textField as? CustomTextField {
+                customTextField.textFieldStatus = .correct
+            }
+        } else {
+            if let customTextField = textField as? CustomTextField {
+                customTextField.textFieldStatus = .normal
+            }
+        }
+    }
+}
+
+
+extension ExperienceLetterViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentText = textField.text ?? ""
+        let newText = (currentText as NSString).replacingCharacters(in: range, with: string)
+        switch textField {
+        case experienceLetterView.recipientNameTextField,experienceLetterView.senderTextField:
+            validateTextField(textField: textField, isValid: newText.isOnlyKorean())
+        case experienceLetterView.phoneFirstText:
+            if newText.count > 3 {
+                return false
+            }
+            validateTextField(textField: textField, isValid: newText == "010")
+        case experienceLetterView.phoneMidText, experienceLetterView.phoneEndText:
+            if newText.count > 4 {
+                return false
+            }
+            validateTextField(textField: textField, isValid: newText.isValidPhoneNumber())
+        default:
+            break
+        }
+        return true
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        let currentText = textField.text ?? ""
+        if currentText.isEmpty {
+            if let customTextField = textField as? CustomTextField {
+                customTextField.textFieldStatus = .normal
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.becomeFirstResponder()
     }
 }
